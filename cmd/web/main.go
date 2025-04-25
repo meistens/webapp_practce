6f81d7aa946a293042c6e70574518250ad3c25ec
David Mebo <mebodave@gmail.com>
2025-04-25 22:20:17 +0100

    Extract routes into their own dedicated file, cleaned up comments for em (step-by-step by checking commit history)

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -14,48 +14,23 @@ type application struct {
 	infoLog *log.Logger
 }
 
+// copy-paste, check VC for how it came to this
 func main() {
-	// define a cmd-line flag with name addr, a default value of 4000
-	// and some help text
-	// parse command to... its obvious what it does...
-	addr := flag.String("addr", ":4000", "http network address")
+	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
-
-	// log.New() for creating a logger for wrting info. msgs
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
-	// same as infoLog, but stderr is the dest.
 	errLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
-
-	// init. a new instance of the application struct
 	app := &application{
 		errLog:  errLog,
 		infoLog: infoLog,
 	}
-
-	// swap the route declarations to use the application struct method as the
-	// handler function
-	mux := http.NewServeMux()
-
-	// create a file server which uses files from the static dir.
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	// use the mux.Handle() to register the file server as the handler
-	// for all url path that starts with "/static/"
-	// for matching paths, strip the /static before the req reaches the fileserver
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
-
-	// init. a new http.Server struct
-	// setup addr and handler so the server uses the same network as before
-	// set errLog so server uses custom error logger
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errLog,
-		Handler:  mux,
+		// Call the new app.routes() method to get the servemux containing our routes.
+		Handler: app.routes(),
 	}
-
-	infoLog.Printf("\nstarting server on %s", *addr)
+	infoLog.Printf("Starting server on %s", *addr)
 	err := srv.ListenAndServe()
 	errLog.Fatal(err)
 }
